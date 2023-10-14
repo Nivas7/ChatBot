4ce8f7461b4d934287ca57f86ec98accbd62e562
Nivas <nivasniyas24@gmail.com>
2023-10-14 18:53:43 +0530

    fixing errors

--- Chatbot/Web/components/logo.tsx
+++ Chatbot/Web/components/logo.tsx
@@ -9,7 +9,7 @@ const LogoBox = () => {
         <div className="flex justify-between bg-gray-800 m-[2rem]">
             <Image src={logo}  className="w-[7rem] object-contain" alt="logo"/>
 
-            {session && <img src={profileImg} onClick={() => signOut()} alt="Profile" className="h-12 w-12 cursor-pointer mb-2 hover:opacity-50 rounded-[50%]" />}
+            {session && <img src={profileImg || ""} onClick={() => signOut()} alt="Profile" className="h-12 w-12 cursor-pointer mb-2 hover:opacity-50 rounded-[50%]" />}
         </div>
      );
 }
