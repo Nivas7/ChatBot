a094ccdbd4856814c1816a8d0e49f8eca13e14ed
Srinivas <93144092+Nivas7@users.noreply.github.com>
2023-10-15 11:49:21 +0530

    Update HomePage.tsx

--- Chatbot/Web/components/HomePage.tsx
+++ Chatbot/Web/components/HomePage.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useRouter } from "next/navigation";
-import Quote from "./Quote";
+
 
 const HomePage = () => {
 
@@ -10,7 +10,7 @@ const HomePage = () => {
         <div>
           <div>
             <h1 className="text-custom mx-[1rem_!important]">Your AI companion&sbquo; ready to chat&sbquo; learn&sbquo; and assist all in one place.</h1>
-            <p className="lg:m-24 md:text-base lg:mx-[20vw] text-gray-400 text-center text-sm lg:text-xl  px-[3rem] m-[3rem]">Welcome to Carla&sbquo; your virtual assistant! Whether you&apos;re a  enthusiast&sbquo; or just looking for information about the Machine learning we&apos;re here to help you. Carla is your go to chatbot for all things automation.</p>
+            <p className="lg:m-24 md:text-base lg:mx-[20vw] text-gray-400 text-center text-sm lg:text-xl  px-[1rem] m-[2rem]">Welcome to Carla&sbquo; your virtual assistant! Whether you&apos;re a  enthusiast&sbquo; or just looking for information about the Machine learning we&apos;re here to help you. Carla is your go to chatbot for all things automation.</p>
 
           </div>
           <button className="bg-blue-800 w-[30vh] md:ml-[30%] lg:ml-[40%] p-[1rem] ml-[20%] mt-[2rem] rounded-lg" onClick={() => {
