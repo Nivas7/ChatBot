4bf98de55d350a823debe769f2940a6fb98188f3
Srinivas <93144092+Nivas7@users.noreply.github.com>
2023-10-15 11:42:58 +0530

    Update HomePage.tsx

--- Chatbot/Web/components/HomePage.tsx
+++ Chatbot/Web/components/HomePage.tsx
@@ -9,16 +9,13 @@ const HomePage = () => {
     return ( 
         <div>
           <div>
-            <h1 className="text-custom">Your AI companion&sbquo; ready to chat&sbquo; learn&sbquo; and assist – all in one place.</h1>
-            <p className="text-xl leading-loose m-24">Welcome to Carla&sbquo; your virtual assistant! Whether you&apos;re a  enthusiast&sbquo; or just looking for information about the Machine learning we&apos;re here to help you. Carla is your go to chatbot for all things automation.</p>
+            <h1 className="text-custom mx-[1rem_!important]">Your AI companion&sbquo; ready to chat&sbquo; learn&sbquo; and assist all in one place.</h1>
+            <p className="lg:m-24 md:text-base lg:mx-[20vw] text-gray-400 text-center text-sm lg:text-xl  px-[3rem] m-[3rem]">Welcome to Carla&sbquo; your virtual assistant! Whether you&apos;re a  enthusiast&sbquo; or just looking for information about the Machine learning we&apos;re here to help you. Carla is your go to chatbot for all things automation.</p>
 
           </div>
-          <button className="bg-blue-800 w-[30vh] p-[1rem] ml-[25vh] rounded-lg" onClick={() => {
+          <button className="bg-blue-800 w-[30vh] md:ml-[30%] lg:ml-[40%] p-[1rem] ml-[20%] mt-[6rem] rounded-lg" onClick={() => {
             router.push("Chat")
           }}>try Carla for free</button>
-          <div className="w-[70%] bg-gray-500 bottom-[2rem] ml-[12vh] mt-[5rem] rounded-lg">
-
-        </div>
         </div>
      );
 }
