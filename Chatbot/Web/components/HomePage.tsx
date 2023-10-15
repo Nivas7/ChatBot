4cea8fd71c9bf4c74deeccebebcaf4ed4d0abd42
Srinivas <93144092+Nivas7@users.noreply.github.com>
2023-10-15 11:04:20 +0530

    Update HomePage.tsx

--- Chatbot/Web/components/HomePage.tsx
+++ Chatbot/Web/components/HomePage.tsx
@@ -9,8 +9,8 @@ const HomePage = () => {
     return ( 
         <div>
           <div>
-            <h1 className="text-custom">Your AI companion, ready to chat, learn, and assist – all in one place.</h1>
-            <p className="text-xl leading-loose m-24">Welcome to Carla, your virtual assistant! Whether you're a  enthusiast, or just looking for information about the Machine learning we're here to help you. Carla is your go-to chatbot for all things automotive.</p>
+            <h1 className="text-custom">Your AI companion&sbquo; ready to chat&sbquo; learn&sbquo; and assist – all in one place.</h1>
+            <p className="text-xl leading-loose m-24">Welcome to Carla&sbquo; your virtual assistant! Whether you&apos;re a  enthusiast&sbquo; or just looking for information about the Machine learning we&apos;re here to help you. Carla is your go to chatbot for all things automation.</p>
 
           </div>
           <button className="bg-blue-800 w-[30vh] p-[1rem] ml-[25vh] rounded-lg" onClick={() => {
